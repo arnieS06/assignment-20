b112587a704c4ce39416f47e24754ceec3ecee01
Arnav Sharma <sharma.arnav2016@gmail.com>
2024-10-16 22:28:08 -0700

    forgot std::

--- printSum.cpp
+++ printSum.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
 void printSum(int inputSum) {
-	std::cout << "\nThe sum of your inputs was: " << inputSum << endl;
+	std::cout << "\nThe sum of your inputs was: " << inputSum << std::endl;
 }
